db52aa4bcaeebbe02edbdcf4794ade072e051efa
Aula72 <simaulat@gmail.com>
2022-07-02 15:12:04 +0300

    assets and header file

--- kebera/incs/foot.php
+++ kebera/incs/foot.php
@@ -1,4 +1,22 @@
 
+</div>
 </main>
+<script src="<?php echo BASE_URL; ?>/assets/js/theme-js/core/popper.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/assets/js/theme-js/core/bootstrap.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/assets/js/theme-js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="<?php echo BASE_URL; ?>/assets/js/theme-js/plugins/smooth-scrollbar.min.js"></script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="<?php echo BASE_URL; ?>/assets/js/theme-js/soft-ui-dashboard.min.js?v=1.0.3"></script>
 </body>
 </html>
--- kebera/incs/head.php
+++ kebera/incs/head.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo BASE_URL; ?>/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="<?php echo BASE_URL; ?>/assets/img/favicon.png">
   <title>
     KeBERA
   </title>
@@ -13,7 +13,7 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
   <link href="<?php echo BASE_URL; ?>/assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="<?php echo BASE_URL; ?>/assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
   <link href="<?php echo BASE_URL; ?>/assets/css/nucleo-svg.css" rel="stylesheet" />
