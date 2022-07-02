bbcbdbfa61602b7f47e564acc071bb369ca3fe89
Aula72 <30939289+Aula72@users.noreply.github.com>
2022-07-02 12:24:57 +0300

    Update index.php

--- kebera/index.php
+++ kebera/index.php
@@ -1,51 +1,11 @@
 <?php 
-$p = $_SERVER;
-// var_dump($p);
-// echo "<br />";
-$url = $p['REQUEST_URI'];
-$p  = explode('/', $_SERVER['REQUEST_URI']);
+include_once "config/sessions.php";
+include_once "config/constants.php";
 
+include_once "incs/head.php";
+include_once "incs/navbar.php";
+include_once "incs/sidebar.php";
 
+include_once "config/routes.php";
 
-switch($url){
-    case "/":
-        include_once "view-normal/index.php";
-        break;
-    case "/login":
-        include_once "./login.php";
-        break;
-    case "/logout":
-        include_once "./logout.php";
-        break;
-    case "/products":
-        include_once "view-normal/products/index.php";
-        break;
-    case "/products/".$p[2]:
-        include_once "view-normal/products/details.php";
-        break;
-    case "/farmers":
-        include_once "view-normal/farmer/index.php";
-        break;
-    case "/farmer-groups":
-        include_once "view-normal/farmer-group/index.php";
-        break;
-    case "/farmer-groups/".$p[2]:
-        include_once "view-normal/farmer-group/details.php=45";
-        break;
-        
-    case "/remember-password":
-        include_once "remember.php";
-        break;
-    case "/register":
-        include_once "register.php";
-        break;
-    
-
-    //dash board routes
-    case "/dashboard":
-        include_once "view-normal/index.php";
-        break;
-    default:
-        include_once "404.php";
-        break;
-}
+include_once "incs/foot.php";
