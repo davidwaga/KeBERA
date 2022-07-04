4e2fe99ae24ede2c86dc4761439a82ffdabc8199
Aula72 <simaulat@gmail.com>
2022-07-04 13:40:40 +0300

    db

--- kebera/config/constants.php
+++ kebera/config/constants.php
@@ -2,7 +2,7 @@
 
 define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST']);
 define('DBHOST', '127.0.0.1');
-define('DBNAME','blogs');
+define('DBNAME','kebera');
 define('DBPASS','');
 define('DBUSER','root');
 define('TOKEN', '');
