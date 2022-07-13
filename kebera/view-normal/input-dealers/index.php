7063fa70f921527bdd0f5cff19609fd1672fb16e
davidwaga <davidwaga75@gmail.com>
2022-07-13 15:11:17 +0300

    [Added] UI list tables

---
+++ kebera/view-normal/input-dealers/index.php
@@ -0,0 +1,17 @@
+
+<div id="input_dealers-list">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>Location</td>
+                <td>User</td>
+                <td>Created by</td>
+                <td>Created On</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody id="input_dealers-table"></tbody>
+    </table>
+</div>
+<script src="assets/js/input_dealers.js"></script>
---
+++ kebera/view-normal/researcher/index.php
@@ -0,0 +1,18 @@
+
+<div id="researcher-list">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>Name</td>
+                <td>Location</td>
+                <td>No. Members</td>
+                <td>Created by</td>
+                <td>Created On</td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody id="research-table"></tbody>
+    </table>
+</div>
+<script src="assets/js/research.js"></script>
